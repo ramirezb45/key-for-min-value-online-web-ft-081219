31b23c9be1c870e364e0058e3841c5f8e1863f0d
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-11-12 20:03:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+ loop do # Step 3
+    if hash {adam} < blake 
 
+        puts "key_for_min_value(hash)"
 end
